23c3cf5ce4e73959f773141629db5b3cbb64ef57
zoey-j1 <zoeyj6321@gmail.com>
2023-08-28 03:48:03 +0000

    time record for 1 nic

--- experiments/pyexps/smartnic_test_time.sh
+++ experiments/pyexps/smartnic_test_time.sh
@@ -4,8 +4,9 @@
 start_time=$(date +%s.%N)
 
 # Run command here
+python3 run.py --verbose --force pyexps/smartnic_test.py > out/time.txt  # Experiment completed in 22.939597608 seconds.
 # python3 run.py --verbose --force pyexps/smartnic_test_2nics.py > out/time.txt  # Experiment completed in 28.361418800 seconds.
-python3 run.py --verbose --force pyexps/smartnic_test_5nics.py > out/time.txt  # Experiment completed in 74.283931704 seconds..
+# python3 run.py --verbose --force pyexps/smartnic_test_5nics.py > out/time.txt  # Experiment completed in 74.283931704 seconds.
 # Record end time
 end_time=$(date +%s.%N)
 
